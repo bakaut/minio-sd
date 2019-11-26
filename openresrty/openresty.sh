de390755a9aa783254efca4447164d9e5dc90601
Nikolay Lebedev <fifo.mail@gmail.com>
2019-11-26 23:14:04 +0300

    open config work

--- openresrty/openresty.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-yum install -y yum-utils
-yum-config-manager --add-repo https://openresty.org/package/centos/openresty.repo
-yum update -y 
-yum install openresty openresty-resty -y
-
-systemctl status openresty && systemctl enable openresty && systemctl start openresty
-
-#/usr/local/openresty/nginx/conf/
---
+++ openresty/openresty.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+yum update -y
+yum install -y yum-utils epel-release
+yum-config-manager --add-repo https://openresty.org/package/centos/openresty.repo
+yum update -y 
+yum install openresty openresty-resty -y
+
+yum-config-manager --enable rhui-REGION-rhel-server-extras rhui-REGION-rhel-server-optional
+yum install certbot python2-certbot-nginx -y
+
+mkdir /usr/local/openresty/nginx/conf/conf.d /usr/local/openresty/nginx/conf/ssl
+cp s3.conf /usr/local/openresty/nginx/conf/conf.d/
+cp /usr/local/openresty/nginx/conf/nginx.conf /tmp/
+rm -rf /usr/local/openresty/nginx/conf/nginx.conf
+cp nginx.conf /usr/local/openresty/nginx/conf/nginx.conf
+cp upstream.conf /usr/local/openresty/nginx/conf/
+
+
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /usr/local/openresty/nginx/conf/ssl/ssl-priv.key -out /usr/local/openresty/nginx/conf/ssl/ssl-pub.crt
+
+/usr/local/openresty/nginx/sbin/nginx -t
+
+
+systemctl status openresty && systemctl enable openresty && systemctl start openresty
+
+
+#/usr/local/openresty/nginx/conf/
+#certbot --nginx
+#sudo certbot --nginx -d example.com -d www.example.com
+#certbot certonly --nginx
+#echo "0 0,12 * * * root python -c 'import random; import time; time.sleep(random.random() * 3600)' && certbot renew" | sudo tee -a /etc/crontab > /dev/null
