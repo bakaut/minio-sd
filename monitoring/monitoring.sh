93f0a07be0a1866ddb4508f8b22cd5cb5ab34482
Nikolay Lebedev <fifo.mail@gmail.com>
2019-12-06 12:52:40 +0300

    Update monitoring.sh

--- monitoring/monitoring.sh
+++ monitoring/monitoring.sh
@@ -3,9 +3,10 @@ yum install -y https://repo.percona.com/yum/percona-release-1.0-12.noarch.rpm
 
 yum install -y pmm-client -y
 
-pmm-admin config --server=192.168.1.42:80  --force && pmm-admin add linux:metrics
+pmm-admin config --server=192.168.1.42:80 --bind-address=`hostname` --client-name=`hostname` --force 
+pmm-admin add linux:metrics
 pmm-admin add external:service  --path=/minio/prometheus/metrics --scheme=https --service-port=443 minio
-#not work.ssl name to setup
+#not work.ssl name to setup https://github.com/percona/pmm-client/blob/master/pmm-admin.go#L1504
 firewall-cmd --zone=public --add-port=42000/tcp --permanent
 firewall-cmd --zone=public --add-port=443/tcp --permanent
 firewall-cmd --reload
