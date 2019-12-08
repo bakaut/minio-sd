0ff284d9dd6147db3137d422269207a8af7c4d9f
Nikolay Lebedev <fifo.mail@gmail.com>
2019-12-08 19:11:03 +0300

    shoud be test

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@
 #set -x
 
 #install 4 consul note to create consul quorum
-#for i in 51 52 53 54 20 25;do bash install_consul.sh 192.168.1.$i;done
-#sleep 10
+##for i in 51 52 53 54 20 25;do bash install_consul.sh 192.168.1.$i;done
+##sleep 30
 #install minio on 4 hosts
-#rm -rf /tmp/bootstrap.lock
-#for i in 51 52 53 54;do bash install_minio.sh 192.168.1.$i;done
+##rm -rf /tmp/bootstrap.lock
+##for i in 51 52 53 54;do bash install_minio.sh 192.168.1.$i;done
 
 #install consul on other minio host
 #for i in 54 55 56 57 58;do bash install_consul.sh 192.168.1.$i;done
@@ -15,12 +15,12 @@
 #for i in 54 55 56 57 58;do bash install_minio.sh 192.168.1.$i;done
 
 #install monitoring on all hosts
-#for i in 51 52 53 54;do bash install_monitoring.sh 192.168.1.$i;done
+##for i in 51 52 53 54 20 25;do bash install_monitoring.sh 192.168.1.$i;done
 
 #install balancer
-bash  install_consul.sh 192.168.1.20
-bash  install_openresty.sh 192.168.1.20
+#bash  install_consul.sh 192.168.1.20
+##bash  install_openresty.sh 192.168.1.20
 
 #Install minio gateway
 #bash install_consul.sh 192.168.1.25
-#bash install_minio_gw.sh 192.168.1.25
+bash install_minio_gw.sh 192.168.1.25
