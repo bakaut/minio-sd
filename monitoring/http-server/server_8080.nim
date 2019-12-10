4cf584a14e1108137a3bafe219c6360af5f86106
Nikolay Lebedev <fifo.mail@gmail.com>
2019-12-10 12:55:56 +0300

    add exception handle

--- monitoring/http-server/server_8080.nim
+++ monitoring/http-server/server_8080.nim
@@ -4,12 +4,22 @@ var server = newAsyncHttpServer()
 
 proc cb(req: Request) {.async.} =
   var p = paramStr(1)
-  var file = openAsync($p, fmRead)
-  file.setFilePos(0)
-  let data = await file.readAll()
-  let headers = newHttpHeaders([("Content-Type","application/json")])
-  let jsonNode = parseJson($data)
-  await req.respond(Http200, $jsonNode, headers)
-  file.close()
+  try:
+    var file = openAsync($p, fmRead)
+    file.setFilePos(0)
+    let data = await file.readAll()
+    try:
+      let jsonNode = parseJson($data)
+      let headers = newHttpHeaders([("Content-Type","application/json")])
+      await req.respond(Http200, $jsonNode, headers)
+      file.close()
+    except JsonParsingError:
+      echo "Cannot parce json file",$p
+      await req.respond(Http200, "Cannot parse json file " & $p)
+      sleep(1000)
+  except OSError:
+    await req.respond(Http200, "Cannot open file " & $p)
+    echo "Cannot open file ",$p
+    sleep(1000)
 
 waitFor server.serve(Port(8080), cb)
