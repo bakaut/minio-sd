7aa4ff63a0adedb3232f9c43ca3819e34e504849
Nikolay Lebedev <fifo.mail@gmail.com>
2019-12-23 00:14:25 +0300

    proxy-cache-remove+hosts (#18)

--- minio/consul-template.sh
+++ minio/consul-template.sh
@@ -2,13 +2,17 @@
 
 cp minio-config.hcl /etc/consul-template/
 
-cp minio-*.ctmpl /etc/consul-template/ && chown -R consul:consul /etc/consul-template/
+cp *.ctmpl  /etc/consul-template/ && chown -R consul:consul /etc/consul-template/
 
 cp minio-reloader.service /etc/systemd/system/minio-reloader.service
 
 consul-template -once -config=/etc/consul-template/minio-config.hcl
 
 consul-template -template "hosts.ctmpl:/etc/hosts" -once
+
+chown -R minio:consul /home/minio/
+
+echo "* * * * * root consul-template -template \"/etc/consul-template/hosts.ctmpl:/etc/hosts\" -once" >> /etc/crontab
 
 systemctl daemon-reload && systemctl enable minio-reloader && systemctl start minio-reloader && systemctl status minio-reloader
 
