5fc983dbcd2c59e1e3a4a4b548d8c195a64b73f9
Nikolay Lebedev <fifo.mail@gmail.com>
2020-01-04 00:17:52 +0300

    CAP theoreme, consistenty minus, availability plus

--- install.sh
+++ install.sh
@@ -17,7 +17,9 @@ for i in 51 52 53 54;do bash install_monitoring.sh 192.168.1.$i;done
 #bash  install_consul.sh 192.168.1.20
 bash  install_openresty.sh 192.168.1.20
 
-sleep 10
+sleep 20
+
+for i in 51 52 53 54 20;do ssh 192.168.1.$i "sed -i '/hosts.ctmpl/d' /etc/crontab";done
 
 mc config host add current https://s3.teatr-stalker.ru OF4IB02NK06AO4XFJ5IA ciLiEqNKsaT2CxuXReVmMB4se0uMjSV5nimV+M9W
 #mc rb --force current/photo
